1e3987d950608cdfb2403c602698e4e750387059
Smartysidd <Sidd271299@gmail.com>
2023-09-12 20:37:19 +0530

    first commit

--- src/Components/CourseCard.js
+++ src/Components/CourseCard.js
@@ -1,14 +1,29 @@
-import CourseCard from './CourseCard';
-
+import React from "react";
+import { courseAPI } from "./CourseCatalog";
 
 
 function CourseCatalog() {
+  // return (
+  //   <div className="course-catalog">
+  //     {courses.map((course) => (
+  //       <CourseCard key={course.id} course={course} />
+  //     ))}
+  //   </div>
+  // );
   return (
-    <div className="course-catalog">
-      {courses.map(course => (
-        <CourseCard key={course.id} course={course} />
-      ))}
-    </div>
+    <>
+      <courseAPI.Consumer>
+        {(course) => {
+          return (
+            <div className="course-catalog">
+              {course?.map((courses) => (
+                <h2>{courses.title}</h2>
+              ))}
+            </div>
+          );
+        }}
+      </courseAPI.Consumer>
+    </>
   );
 }
-export default CourseCard;
+export default CourseCatalog;
